8eb1ce7ab1257292db1a370f98b86a5e5df24e07
Jayalakshmi D <140176223+jayalakshmi410@users.noreply.github.com>
2024-07-04 09:07:24 +0530

    Add files via upload

---
+++ object_detection_demo.py
@@ -0,0 +1,16 @@
+import cv2
+import matplotlib.pyplot as plt
+config_file="ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+frozen_model="frozen_inference_graph.pg"
+model=cv2.dnn_DetectionModel(frozen_model,config_file)
+classLabels=[]
+file_name="labels.txt"
+with open(file_name,'rt') as fpt:
+    classLabels=fpt.read().rstrip('\n').split('\n')
+print(classLabels)
+print(len(classLabels))
+model.setInputSize(320,320)
+model.setInputScale(1.0/127.5)
+model.setInputMean((127.5,127,5,127.5))
+model.setInputSwapRB(True)
+img=cv2.imread("WindowsLogo.jpg")
